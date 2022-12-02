7957c0428f5ca31e5e6f4f75818fcdf577e39356
Mattia <mat.scotti@studenti.unina.it>
2022-12-02 18:25:22 +0100

    *

--- Client/SrcC/SocketUtilClient.c
+++ Client/SrcC/SocketUtilClient.c
@@ -1,22 +1,21 @@
-int createAndBindClientSocket(){
+int createAndBindClientSocket(struct sockaddr_in* client_addr, int porta, char indirizzo[]){
     int sd1;
-    char buf[50]="Messaggio da parte del client\n";
-    int len=strlen(buf);
-    struct sockaddr_in indirizzo;
-    indirizzo.sin_family=AF_INET;
-    indirizzo.sin_port=htons(5200);
+    struct sockaddr_in client_addr;
+    client_addr.sin_family=AF_INET;
+    client_addr.sin_port=htons(porta);
 
-    inet_aton("127.0.1.1", &indirizzo.sin_addr);
+    inet_aton(indirizzo, &client_addr.sin_addr);
 
     if((sd1=socket(PF_INET, SOCK_STREAM,0))<0){
         perror(":SOCKET ERROR:");
         exit(1);
     }
 
-    if(connect(sd1, (struct sockaddr*)&indirizzo, sizeof(indirizzo))<0) {
+    if(connect(sd1, (struct sockaddr*)&client_addr, sizeof(client_addr))<0) {
         perror(":CONNECT ERROR:");
         close(sd1);
         exit(1);
     }
 
+    return sd1;
 }
