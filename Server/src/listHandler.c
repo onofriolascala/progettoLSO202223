6e0f67bdbee2ed807dea615d77a3657ae488f676
Mattia <mat.scotti@studenti.unina.it>
2022-12-15 15:48:11 +0100

    *

---
+++ Server/src/listHandler.c
@@ -0,0 +1,12 @@
+//
+// Created by Mattia on 15/12/2022.
+//
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+void newHead(struct player_node* head){
+    struct player_node temp;
+    temp=*head;
+    *head=malloc()
+}
