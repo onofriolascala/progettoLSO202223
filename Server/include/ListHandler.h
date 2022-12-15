b6f0e3b2d2f4e4f1bcb188d3816796b6c0593212
onofriolascala <on.lascala@studenti.unina.it>
2022-12-15 18:58:54 +0100

    aggiunta funzioni per la gestione dei player node

--- Server/include/ListHandler.h
+++ Server/include/ListHandler.h
@@ -5,7 +5,11 @@
 #ifndef PROGETTOLSO202223_LISTHANDLER_H
 #define PROGETTOLSO202223_LISTHANDLER_H
 
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
 
+#include "../include/Def.h"
 
 
 #endif //PROGETTOLSO202223_LISTHANDLER_H
--- Server/src/ListHandler.c
+++ Server/src/ListHandler.c
@@ -1,41 +1,77 @@
 //
 // Created by Mattia on 15/12/2022.
 //
-#include <string.h>
-#include <stdlib.h>
-#include <stdio.h>
 
-#include "../include/Def.h"
 
-/*
-void newHead(struct player_node* head){
-    struct player_node temp;
-    temp=*head;
-    *head=malloc()
+#include "../include/ListHandler.h"
+
+
+struct player_node* createNewPlayerNode( int player_socket, char username[], char service_addr[]){
+    struct player_node* player_node;
+    if( (player_node = (struct player_node*)malloc(sizeof(player_node))) == NULL){
+        //gestire errore malloc
+    }
+    else{
+        player_node->player_socket = player_socket;
+        strcpy( player_node->username, username);
+        strcpy( player_node->service_addr, service_addr);
+        player_node->next = NULL;
+    }
+    return player_node;
 }
 
-*/
+struct player_node* removePlayerNode( struct player_node* player_list, int target_socket ){
+    if( player_list != NULL ){
+        if( player_list->player_socket == target_socket ){
+            player_list = player_list->next;
+        }
+        else
+            player_list->next = removePlayerNode(player_list -> next, target_socket);
+    }
+    return player_list;
+}
+
+int destroyPlayerNode( struct player_node* player_node ){
+    int distrutto = -1;
+    if( player_node != NULL ){
+        free(player_node);
+        distrutto = 1;
+    }
+    return distrutto;
+}
+
+
 
 
 struct room_node* addNewRoom(struct room_node* room_list) {
     struct room_node* new;
     if(room_list == NULL){
         new = (struct room_node*)malloc(sizeof(struct room_node));
-        new->id = 1;
-        strcpy(new->localsocket, "");
-        new->player_list = NULL;
-        new->player_num = 0;
-        new->next = NULL;
-        room_list = new; //aggiorno la testa della lista delle stanze
+        if( new != NULL){
+            new->id = 1;
+            strcpy(new->localsocket, "");
+            new->player_list = NULL;
+            new->player_num = 0;
+            new->next = NULL;
+            room_list = new;
+        }
+        else;
+            //gestisci errore malloc
     }
     else{
         new = (struct room_node*)malloc(sizeof(struct room_node));
-        new->id = room_list->id + 1;
-        strcpy(new->localsocket, "");
-        new->player_list = NULL;
-        new->player_num = 0;
-        new->next = room_list;
-        room_list = new; //aggiorno la testa della lista delle stanze
+        if( new != NULL){
+            new->id = room_list->id + 1;
+            strcpy(new->localsocket, "");
+            new->player_list = NULL;
+            new->player_num = 0;
+            new->next = room_list;
+            room_list = new;
+        }
+        else;
+            //gestisci errore malloc
     }
     return room_list;
 }
+
+
