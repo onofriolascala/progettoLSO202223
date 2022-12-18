3c8efc8b0392abc692bb30395669d76f52430c34
al-vecce <al.vecce@studenti.unina.it>
2022-12-18 06:17:41 -0500

    Added Room and Game Logic files

---
+++ Server/include/RoomLogic.h
@@ -0,0 +1,24 @@
+//
+// Created by osboxes on 12/18/22.
+//
+
+#ifndef ROOMLOGIC_H
+#define ROOMLOGIC_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <pthread.h>
+#include <sys/types.h>
+#include <errno.h>
+#include <netinet/in.h>
+#include <sys/un.h>
+#include <string.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <pthread.h>
+
+#include "../include/Def.h"
+
+#endif //ROOMLOGIC_H
---
+++ Server/src/GameLogic.c
@@ -0,0 +1,5 @@
+//
+// Created by osboxes on 12/18/22.
+//
+
+#include "../include/GameLogic.h"
