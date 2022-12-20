0268f6080f1b8163f5b31d9a9ab394224f869de2
Mattia <mat.scotti@studenti.unina.it>
2022-12-20 15:20:35 +0100

    *

--- Server/src/LogicSigninLogin.c
+++ Server/src/LogicSigninLogin.c
@@ -4,58 +4,58 @@
 
 #include "../include/LogicSigninLogin.h"
 
-void sigin(){
-
-}
-
-void logout(){
-
-}
-
-int findUsername(char incoming[])
-{
-    char string[50];
-    int num=0;
-    FILE *in_file = fopen("users.txt", "r");
-    if (in_file == NULL)
-    {
-        printf("Error file missing\n");
-        return 0;
+int signin(char incoming[], struct player_node* player, char outgoing[]){
+    int signal_num;
+    char username[USERNAMELENGHT+1], password[PASSWORDLENGHT+1], temp_buf[MAXSIGNALBUF+1];
+    char* sub_string;
+    int username_len, pswd_len, cursor;
+    strncpy(temp_buf, incoming, MAXSIGNALBUF);
+    temp_buf[MAXSIGNALBUF] = '\0';
+    username_len = atoi(temp_buf);
+    //controlli di correttezza username, implementate richieste di dimensione
+    if((username_len<6)||(username_len>32)){
+        strcpy(outgoing, "L'username non soddisfa le richieste");
+        signal_num = 91;
     }
-    while ( (fscanf(in_file,"%s", string) == 1)&&(num==0))
-    {
-        if(strstr(string, incoming)!=0) {
-            num++;
-            printf("Login successful");
+    else {
+        cursor = MAXSIGNALBUF;
+        sub_string = &incoming[cursor];
+        strncpy(username, sub_string, username_len);
+        cursor += username_len;
+        username[username_len] = '\0';
+        memset(temp_buf, 0, sizeof(temp_buf));
+        sub_string = &incoming[cursor];
+        strncpy(temp_buf, sub_string, MAXSIGNALBUF);
+        temp_buf[MAXSIGNALBUF] = '\0';
+        pswd_len = atoi(temp_buf);
+        //controlli di correttezza password, implementate richieste di dimensione, necessarie regole per la scelta di caratteri?
+        if((pswd_len<6)||(pswd_len>32)){
+            strcpy(outgoing, "La password non soddisfa le richieste");
+            signal_num = 91;
+        }
+        else {
+            cursor += MAXSIGNALBUF;
+            sub_string = &incoming[cursor];
+            strncpy(password, sub_string, pswd_len);
+            password[pswd_len] = '\0';
+            if (1/*insertQuery((struct mySQLConnection* LSO2223, char[] username, char[] password):)*/== 1062) {
+                strcpy(outgoing, "Utente già esistente.");
+                signal_num = 91;
+            } else {
+                strcpy(outgoing, "Utente registrato.");
+                signal_num = 51;
+            }
         }
     }
-    fclose(in_file);
-    return 1;
+    return signal_num;
 }
 
-int login_old(char incoming[], struct player_node* player, char outgoing[]){
+struct player_node* logout(struct player_node* player){
     int signal_num;
-    //DEPRECATO //controllo coerenza dati trasmessi dal client, username e password che non utilizzano caratteri non accettati ed appartengono al range accettato 6-32
-    //Ritengo più efficiente fare un cotrollo di coerenza dati all'inserimento, piuttosto che fare un parsing della concatenzaione di username e password
-    //In questa funzione verrà considerato che nel file, le credenziali di ogni utente, sono conservate come una stringa concatenata di username e password
-    if(findUsername(&incoming[1])){
-        int i;
-        char* username;
-        char* userNum;
-        memcpy(userNum, incoming, 1);
-        i=atoi(userNum);
-        memcpy(username, incoming, i);
-        username[i] = '\0';
-        printf("nome utente %s ", username);
-        player=createNewPlayerNode(12, username);
-        strcpy(outgoing,"Login successful");
-        signal_num=52;
-    }
-    else{
-        strcpy(outgoing,"Login error");
-        signal_num=91;
-    }
-    return signal_num;
+    signal_num=51;
+    destroyPlayerNode(player);
+    player=NULL;
+    return player;
 }
 
 int login(int sd, char incoming[], struct player_node* player, char outgoing[]) {
